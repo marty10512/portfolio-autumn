c4c8e1a3923729649913004b1a201f5e22d04abe
Andrey Martemyanov <marty105k@gmail.com>
2019-10-14 15:58:05 +0300

    build fix

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = '/ls-advansed-portfolio.github.io/dist/';
+  const publicPath = '/portfolio.github.io/tree/gh-pages/';
 
   const pcss = {
     test: /\.(p|post|)css$/,
