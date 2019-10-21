d6d04e07998e65df543d3b9d5bfdabc7c0ca37ad
Andrey Martemyanov <marty105k@gmail.com>
2019-10-21 18:35:35 +0300

    parallax done

---
+++ src/scripts/parallax.js
@@ -0,0 +1,19 @@
+(function(){
+  const parallax = document.querySelector('.parallax');
+  const layers = parallax.children;
+
+  function moveLayersDependsOnScroll(wScroll) {
+    Array.from(layers).forEach(layer => {
+
+      const divider = layer.dataset.speed;
+      const strafe = wScroll * divider / 20;
+
+      layer.style.transform = `translateY(-${strafe}%) translateZ(0)`;
+    })
+  }
+
+  window.addEventListener('scroll', e => {
+    const wScroll = window.pageYOffset;
+    moveLayersDependsOnScroll(wScroll);
+  })
+})();
